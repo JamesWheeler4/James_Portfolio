2c9f0b0706ab42d3469530ffb99d4fe5cf70adfa
JamesWheeler4 <88292886+JamesWheeler4@users.noreply.github.com>
2022-07-07 16:25:04 -0700

    Create graphWomenCor.py

---
+++ Proj_5 Track and Field Scouting/graphWomenCor.py
@@ -0,0 +1,18 @@
+# Creating women's heatmap
+def graphWomenCor():
+    df = pd.read_csv('Womens_HM_data.csv', header=None)
+    df = df.values.tolist()
+    fig = px.imshow(df,
+                    labels=dict(x="Event x", y="Event y", color="Correlation Coefficient"),
+                    x=['100m', '200m', '400m', '800m', '1500m', '3000m', 'high jump', '100m hurdles', '300m hurdles',
+                       '1600m', "3200m", 'shot put', 'discus', 'javelin', 'hammer', 'pole vault', 'long jump',
+                       'triple jump'],
+                    y=['100m', '200m', '400m', '800m', '1500m', '3000m', 'high jump', '100m hurdles', '300m hurdles',
+                       '1600m', "3200m", 'shot put', 'discus', 'javelin', 'hammer', 'pole vault', 'long jump',
+                       'triple jump'],
+                    text_auto=True, color_continuous_scale='balance'
+                   )
+
+    fig.update_layout(title=('Womens Highschool Track and Field Correlation Coefficient'), title_font_size=32,
+                      title_x=0.5, paper_bgcolor='#f4f0e4')
+    fig.show()
